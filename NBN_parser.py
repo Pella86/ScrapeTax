01ab1c471a1f641ad9c80d35da94f5766ae3b81b
Pella86 <pigmeo127@gmail.com>
2020-04-02 12:02:19 +0200

    Improved the function that looks for the species link, soon maybe it will be able to scrap the whole family

--- NBN_parser.py
+++ NBN_parser.py
@@ -257,6 +257,71 @@ def create_authority_line(n_base, specie, base_path):
     return n_base, lines
 
 
+def get_rank(dt):
+    return dt.text
+
+def get_link(dd):
+    link = dd.find("a")
+    if link:
+        return NBN_HOME + link.get("href")
+    else:
+        return None
+    
+def get_name(dd):
+    return dd.find("span", class_="name").text
+
+def generate_filename(base_folder, prefix, name):
+    filename = f"{prefix}_{name}_webpage.pickle" 
+    return os.path.join(base_folder, filename)
+
+def get_species_tags(tdt, tdd, base_folder, prefix):
+    if get_rank(tdt) == "genus":
+        link = get_link(tdd)
+        if link:
+            name = get_name(tdd)
+            filename = generate_filename(base_folder, prefix, name)    
+            return gather_child_taxa(link, filename)
+        
+        
+class NBNElement:
+    
+    
+    def __init__(self, dt, dd):
+        self.html_rank = dt
+        self.html_name = dd
+    
+    def get_link(self):
+        link = self.html_name.find("a")
+        if link:
+            return NBN_HOME + link.get("href")
+        else:
+            return None  
+    
+    def get_name(self):
+        return self.html_name.find("span", class_="name").text
+    
+    def get_rank(self):
+        return self.html_rank.text
+    
+    def generate_filename(self, base_folder, prefix):
+        filename = f"{prefix}_{self.get_name()}_webpage.pickle" 
+        return os.path.join(base_folder, filename)    
+
+    def gather_child_elements(self, base_folder, prefix):
+        if self.get_link():
+            link = NBN_HOME + self.get_link()
+            filename = self.generate_filename(base_folder, prefix)
+            html_elements = gather_child_taxa(link, filename)
+            return [NBNElement(dt, dd) for dt, dd in html_elements]
+    
+    def get_author(self):
+        return self.html_name.find("span", class_="author").text
+
+
+
+    
+    
+            
 
 if __name__ == "__main__":
     # load family home page
@@ -266,64 +331,176 @@ if __name__ == "__main__":
     
     url = "https://species.nbnatlas.org/species/NBNSYS0000160829"
     
-    filename = os.path.join(base_folder, prefix + "_genus" + "_website" + ".pickle")
+    filename = generate_filename(base_folder, prefix, "family")
     
-    soup = get_soup(url, filename)
+    # change the Taxa to just a list of genus elements
+    # then build the genus list and species list
     
-    # search for the child taxa section that contains all the names
-    children = soup.find("dl", class_="child-taxa")
-    
-    # names with hierarchy tags (subfamily, genus, ...)
-    dts = children.find_all("dt")
-    
-    # the real names
-    dds = children.find_all("dd")
-
-    
-    ranks = ["unranked", "kingdom", "phylum", "subphylum", "class", "order", "family", "subfamily", "tribe", "genus", "species"]
-    
-    subfamily_count = 0
-    tribe_count = 0
-    
-    species_list = ""
-    
+    taxa = gather_child_taxa(url, filename)
     
     genus_list = []
+    species_list = []
     
-    for dt, dd in zip(dts, dds):
-
-        if dt.text == "subfamily":
-            slink = dd.find("a")
-            
-            if slink:
-                slink = slink.get("href")
-                name = dd.find("span", class_="name").text
-                
-                subfamily_count += 1
-
-                tags = gather_child_taxa(NBN_HOME + slink, os.path.join(base_folder, prefix + "_" + name + "_" + str(subfamily_count) + ".pickle" ))
-                
-                
-                
-                for sdt, sdd in tags:
-                    print("-"*79)
-                    
-                    if sdt.text == "tribe":
-                        tlink = sdd.find("a")
-                        if tlink:
-                            tlink = tlink.get("href")
-                            
-                            name = sdd.find("span", class_="name").text
-                            print(name)
-                            
-                            tribe_tags = gather_child_taxa(NBN_HOME + tlink, os.path.join(base_folder, prefix + "_" + name + "_" + str(tribe_count) + ".pickle" ))
-                            
-                            for tdt, tdd in tribe_tags:
-                                print(tdt.text + ": " + tdd.find("span", class_="name").text)
+    for dt, dd in taxa:
         
-        if dt.text == "genus":
-            link = dd.find("a").get("href")
-            pass
+        element = NBNElement(dt, dd)
+        
+        print(element.get_rank())
+        
+        if element.get_rank() == "subfamily":
+            
+            subfam = element.gather_child_elements(base_folder, prefix)
+            
+            
+            for subf in subfam:
+                print(subf.get_name())
+                
+                if subf.get_rank() == "tribe":
+                    genuses = subf.gather_child_elements(base_folder, prefix)
+                    
+                    for genus in genuses:
+                        if genus.get_rank() == "genus":
+                            name = genus.get_name()
+                            author = genus.get_author()
+                        
+                            genus_list.append(Taxa.Taxa(name, author, genus.get_link(), "none"))
+                
+                if subf.get_rank() == "genus":
+                    # get name, get author
+                    name = subf.get_name()
+                    author = subf.get_author()
+                    
+                    genus_list.append(Taxa.Taxa(name, author, subf.get_link(), "none"))
+        
+        if element.get_rank() == "genus":
+            # get name, get author
+            name = element.get_name()
+            author = element.get_author()
+            
+            genus_list.append(Taxa.Taxa(name, author, element.get_link(), "none"))
+                              
+    
+    for genus in genus_list:
+        print(genus, genus.link)
+        
+        
+                                      
+            
+            
+            
+    
+    
+    
+    # tags = gather_child_taxa(url, filename)
+
+    # species_list = []
+
+    # for dt, dd in tags:
+        
+    #     if get_rank(dt) == "subfamily":
+    #         link = get_link(dd)
+    #         if link:
+    #             name = get_name(dd)
+    #             filename = generate_filename(base_folder, prefix, name)
+                
+    #             sub_tags = gather_child_taxa(link, filename)
+                
+    #             for sdt, sdd in sub_tags:
+                    
+    #                 if get_rank(sdt) == "tribe":
+    #                     link = get_link(sdd)
+    #                     if link:
+    #                         name = get_name(sdd)
+    #                         filename = generate_filename(base_folder, prefix, name)
+                            
+    #                         tribe_tags = gather_child_taxa(link, filename)
+                            
+    #                         for tdt, tdd in tribe_tags:
+                                
+    #                             if get_rank(tdt) == "genus":
+    #                                 link = get_link(tdd)
+    #                                 if link:
+    #                                     name = get_name(tdd)
+    #                                     filename = generate_filename(base_folder, prefix, name)
+                            
+    #                                     genus_tags = gather_child_taxa(link, filename)
+                                        
+    #                                     for gdt, gdd in genus_tags:
+                                            
+    #                                         link = get_link(gdd)
+                                            
+    #                                         if link:
+    #                                             name = get_name(gdd)
+    #                                             species = gather_taxonomy(link, generate_filename(base_folder, prefix, name))
+                                                
+    #                                             print(species)
+                                            
+                                                           
+                                    
+                                    
+                    
+                
+                
+            
+    
+    
+    # soup = get_soup(url, filename)
+    
+    # # search for the child taxa section that contains all the names
+    # children = soup.find("dl", class_="child-taxa")
+    
+    # # names with hierarchy tags (subfamily, genus, ...)
+    # dts = children.find_all("dt")
+    
+    # # the real names
+    # dds = children.find_all("dd")
+
+    
+    # ranks = ["unranked", "kingdom", "phylum", "subphylum", "class", "order", "family", "subfamily", "tribe", "genus", "species"]
+    
+    # subfamily_count = 0
+    # tribe_count = 0
+    
+    
+    # species_list = ""
+    
+    
+    # genus_list = []
+    
+    # for dt, dd in zip(dts, dds):
+
+    #     if dt.text == "subfamily":
+    #         slink = dd.find("a")
+            
+    #         if slink:
+    #             slink = slink.get("href")
+    #             name = dd.find("span", class_="name").text
+                
+    #             subfamily_count += 1
+
+    #             tags = gather_child_taxa(NBN_HOME + slink, os.path.join(base_folder, prefix + "_" + name + "_" + str(subfamily_count) + ".pickle" ))
+                
+                
+                
+    #             for sdt, sdd in tags:
+    #                 print("-"*79)
+                    
+    #                 if sdt.text == "tribe":
+    #                     tlink = sdd.find("a")
+    #                     if tlink:
+    #                         tlink = tlink.get("href")
+                            
+    #                         name = sdd.find("span", class_="name").text
+    #                         print(name)
+                            
+    #                         tribe_tags = gather_child_taxa(NBN_HOME + tlink, os.path.join(base_folder, prefix + "_" + name + "_" + str(tribe_count) + ".pickle" ))
+                            
+    #                         for tdt, tdd in tribe_tags:
+    #                             print(tdt.text + ": " + tdd.find("span", class_="name").text)
+        
+    #     if dt.text == "genus":
+    #         link = dd.find("a").get("href")
+    #         pass
             
                        
                             
